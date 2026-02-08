257951f6fc40cf426746572f675e4502953d0707
anaximeno <anaximenobrito@gmail.com>
2026-02-08 14:40:51 -0100

    Add support for singaling mouse clicks

--- src/core/display.c
+++ src/core/display.c
@@ -167,6 +167,7 @@ enum
   INIT_XSERVER,
   ZOOM_SCROLL_IN,
   ZOOM_SCROLL_OUT,
+  BUTTON_PRESSED,
   LAST_SIGNAL
 };
 
@@ -563,6 +564,13 @@ meta_display_class_init (MetaDisplayClass *klass)
                   G_SIGNAL_RUN_LAST,
                   0, NULL, NULL, NULL,
                   G_TYPE_NONE, 0);
+
+  display_signals[BUTTON_PRESSED] =
+    g_signal_new ("button-pressed",
+                  G_TYPE_FROM_CLASS (klass),
+                  G_SIGNAL_RUN_LAST,
+                  0, NULL, NULL, NULL,
+                  G_TYPE_NONE, 1, CLUTTER_TYPE_EVENT);
 
   g_object_class_install_property (object_class,
                                    PROP_FOCUS_WINDOW,
